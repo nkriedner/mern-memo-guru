ebec5867984b65409ff0506c2b01379586d8350d
Nils Kriedner <nkriedner@googlemail.com>
2022-08-18 10:25:11 +0200

    Create the flip card effect in Train Component

--- frontend/src/components/Train.js
+++ frontend/src/components/Train.js
@@ -1,5 +1,31 @@
+// import { useState } from "react";
+
 const Train = () => {
-    return <h1>Inside Train Component</h1>;
+    const toggleCard = (e) => {
+        console.log("toggle card clicked...");
+
+        // Toggle 'is-flipped' class on eacht click:
+        document.getElementsByClassName("card-inner")[0].classList.toggle("is-flipped");
+    };
+
+    const nextCard = () => {
+        console.log("clicked for next card...");
+    };
+
+    return (
+        <div>
+            <h1>Train</h1>
+            <div className="card-container">
+                <div onClick={(e) => toggleCard(e)} className="card-inner">
+                    <div className="card question">QUESTION</div>
+                    <div className="card answer">ANSWER</div>
+                </div>
+            </div>
+            <button onClick={nextCard} className="nxt-btn">
+                NEXT
+            </button>
+        </div>
+    );
 };
 
 export default Train;
