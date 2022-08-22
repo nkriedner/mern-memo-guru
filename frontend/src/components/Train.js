041727e06c2faa8a3b966aa8eb6b8e9c6831d911
Nils Kriedner <nkriedner@googlemail.com>
2022-08-22 17:39:55 +0200

    Use useEffect() hook for setting the memoLists + fix multiple other bugs

--- frontend/src/components/Train.js
+++ frontend/src/components/Train.js
@@ -6,13 +6,7 @@ const Train = () => {
     const [currentCard, setCurrentCard] = useState(null);
     const [currentQuestion, setCurrentQuestion] = useState("START ⬇️");
     const [currentAnswer, setCurrentAnswer] = useState("START ⬇️");
-    const [currentMemoLevel, setCurrentMemoLevel] = useState("mix");
-    const [memoListLength1, setMemoListLength1] = useState(0);
-    const [memoListLength2, setMemoListLength2] = useState(0);
-    const [memoListLength3, setMemoListLength3] = useState(0);
-    const [memoListLength4, setMemoListLength4] = useState(0);
-    const [memoListLength5, setMemoListLength5] = useState(0);
-    const [memoListLengthMix, setMemoListLengthMix] = useState(0);
+    const [currentMemoLevel, setCurrentMemoLevel] = useState("Mix");
     const [currentMemoList, setCurrentMemoList] = useState([]);
     const [memoList1, setMemoList1] = useState([]);
     const [memoList2, setMemoList2] = useState([]);
@@ -21,7 +15,6 @@ const Train = () => {
     const [memoList5, setMemoList5] = useState([]);
 
     useEffect(() => {
-        // (useEffect runs when a component renders)
         // To get all cards:
         const fetchCards = async () => {
             const response = await fetch("/api/cards");
@@ -31,36 +24,57 @@ const Train = () => {
                 // Update context state:
                 dispatch({ type: "SET_CARDS", payload: json });
             }
+            updateMemoLists(json);
         };
 
         fetchCards();
     }, []); // the empty array lets it only render once at the beginning
 
-    const toggleCard = (e) => {
+    const toggleCard = () => {
         // Toggle 'is-flipped' class on each click:
         document.getElementsByClassName("card-inner")[0].classList.toggle("is-flipped");
     };
 
-    const nextCard = async () => {
-        await updateCurrentMemoLevel(currentMemoLevel);
-        await updateMemoLists();
-        console.log("currentMemoList:", currentMemoList);
-        console.log("currentMemoLevel:", currentMemoLevel);
-
+    const nextCard = () => {
         // Pick a random card:
-        const randomNumber = Math.floor(Math.random() * currentMemoList.length);
-        const randomCard = currentMemoList[randomNumber];
+        let randomNumber;
+        let randomCard;
+        if (currentMemoList.length === 0) {
+            // If currentMemoList is not set -> use cards (= "Mix")
+            noMoreCards();
+            return;
+        } else {
+            // if currentMemoList is set -> use it
+            randomNumber = Math.floor(Math.random() * currentMemoList.length);
+            randomCard = currentMemoList[randomNumber];
 
-        // Set current card:
-        setCurrentCard(randomCard);
+            setCurrentCard(randomCard); // Set current card
+            setCurrentQuestion(randomCard.content_1); // Set the content in the front card
+            setCurrentAnswer(randomCard.content_2); // Set the content in the back card:
+        }
 
-        // Set the contents in the front and back card:
-        setCurrentQuestion(randomCard.content_1);
-        setCurrentAnswer(randomCard.content_2);
+        updateCurrentMemoLevel(currentMemoLevel);
+        updateMemoLists();
+    };
+
+    const noMoreCards = () => {
+        setCurrentCard(null);
+        setCurrentQuestion("(No card for this memo level. Choose a different memo level 2!)");
+        setCurrentAnswer("(No card for this memo level. Choose a different memo level 2!)");
+        setCurrentMemoList([]);
     };
 
     const memoLevelDown = async () => {
+        let noCardsLeft = false;
+
+        if (!currentCard) {
+            return;
+        }
+
         if (currentCard.memo_level > 1) {
+            if (currentMemoList.length == 1) {
+                noCardsLeft = true;
+            }
             currentCard.memo_level = 1;
         }
 
@@ -74,11 +88,27 @@ const Train = () => {
         });
         const json = await response.json();
 
-        nextCard();
+        updateCurrentMemoLevel(currentMemoLevel);
+        updateMemoLists(cards);
+
+        if (noCardsLeft) {
+            noMoreCards();
+        } else {
+            nextCard();
+        }
     };
 
     const memoLevelUp = async () => {
+        let noCardsLeft = false;
+
+        if (!currentCard) {
+            return;
+        }
+
         if (currentCard.memo_level < 5) {
+            if (currentMemoList.length == 1) {
+                noCardsLeft = true;
+            }
             currentCard.memo_level++;
         }
 
@@ -92,27 +122,34 @@ const Train = () => {
         });
         const json = await response.json();
 
-        // await updateMemoLists();
-        nextCard();
+        updateMemoLists(cards);
+
+        if (noCardsLeft) {
+            noMoreCards();
+        } else {
+            nextCard();
+        }
     };
 
     // Function for setting the memo lists lengths:
-    const updateMemoLists = () => {
-        console.log("updateMemoLists() runs...");
+    const updateMemoLists = (memoLists) => {
+        if (!memoLists) {
+            memoLists = cards;
+        }
 
-        const list1 = cards.filter((val) => {
+        const list1 = memoLists.filter((val) => {
             return val["memo_level"] === 1;
         });
-        const list2 = cards.filter((val) => {
+        const list2 = memoLists.filter((val) => {
             return val["memo_level"] === 2;
         });
-        const list3 = cards.filter((val) => {
+        const list3 = memoLists.filter((val) => {
             return val["memo_level"] === 3;
         });
-        const list4 = cards.filter((val) => {
+        const list4 = memoLists.filter((val) => {
             return val["memo_level"] === 4;
         });
-        const list5 = cards.filter((val) => {
+        const list5 = memoLists.filter((val) => {
             return val["memo_level"] === 5;
         });
         setMemoList1(list1);
@@ -120,17 +157,6 @@ const Train = () => {
         setMemoList3(list3);
         setMemoList4(list4);
         setMemoList5(list5);
-        setMemoListLength1(list1.length);
-        setMemoListLength2(list2.length);
-        setMemoListLength3(list3.length);
-        setMemoListLength4(list4.length);
-        setMemoListLength5(list5.length);
-        setMemoListLengthMix(cards.length);
-    };
-
-    const startToSelect = () => {
-        updateMemoLists();
-        document.querySelector(".select-request").style.display = "none";
     };
 
     const updateCurrentMemoLevel = (theLevelIs) => {
@@ -144,7 +170,7 @@ const Train = () => {
             setCurrentMemoList(memoList4);
         } else if (theLevelIs === "5") {
             setCurrentMemoList(memoList5);
-        } else if (theLevelIs === "M") {
+        } else {
             setCurrentMemoList(cards);
         }
     };
@@ -162,22 +188,10 @@ const Train = () => {
             e.target.classList.add("active-memo-lvl");
             setCurrentMemoLevel(e.target.textContent.charAt(0));
             // Set the current list:
-            console.log("current memo list levl:", e.target.textContent.charAt(0));
             const theLevelIs = e.target.textContent.charAt(0);
             updateCurrentMemoLevel(theLevelIs);
-            // if (theLevelIs === "1") {
-            //     setCurrentMemoList(memoList1);
-            // } else if (theLevelIs === "2") {
-            //     setCurrentMemoList(memoList2);
-            // } else if (theLevelIs === "3") {
-            //     setCurrentMemoList(memoList3);
-            // } else if (theLevelIs === "4") {
-            //     setCurrentMemoList(memoList4);
-            // } else if (theLevelIs === "5") {
-            //     setCurrentMemoList(memoList5);
-            // } else if (theLevelIs === "M") {
-            //     setCurrentMemoList(cards);
-            // }
+
+            // nextCard();
             setCurrentQuestion("START ⬇️");
             setCurrentAnswer("START ⬇️");
         }
@@ -185,18 +199,15 @@ const Train = () => {
 
     return (
         <div className="train-container">
-            <button className="btn select-request" onClick={startToSelect}>
-                Get Cards!
-            </button>
             <h1>Train</h1>
             <div className="select-level-container">
                 <div onClick={(e) => activateMemoLvl(e)} className="lvl-btn-container">
-                    <button className="btn btn-lvl active-memo-lvl">Mix ({memoListLengthMix})</button>
-                    <button className="btn btn-lvl">1 ({memoListLength1})</button>
-                    <button className="btn btn-lvl">2 ({memoListLength2})</button>
-                    <button className="btn btn-lvl">3 ({memoListLength3})</button>
-                    <button className="btn btn-lvl">4 ({memoListLength4})</button>
-                    <button className="btn btn-lvl">5 ({memoListLength5})</button>
+                    <button className="btn btn-lvl active-memo-lvl">Mix ({cards && cards.length})</button>
+                    <button className="btn btn-lvl">1 ({memoList1.length})</button>
+                    <button className="btn btn-lvl">2 ({memoList2.length})</button>
+                    <button className="btn btn-lvl">3 ({memoList3.length})</button>
+                    <button className="btn btn-lvl">4 ({memoList4.length})</button>
+                    <button className="btn btn-lvl">5 ({memoList5.length})</button>
                 </div>
             </div>
 
